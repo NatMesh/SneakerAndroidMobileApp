f2e5950db5e2699fcc67314d4d1da7539c02d366
Nathanael Meshesha <nathanael.meshesha@yahoo.com>
2020-11-16 05:07:47 -0600

    Added bottom navigation bar, created classes for our activities favourites, search, news, and profile. Also designed layout files for each of our new activities.

--- app/src/main/java/com/example/sneakersandroidmobileapp/MainActivity.java
+++ app/src/main/java/com/example/sneakersandroidmobileapp/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.sneakersandroidmobileapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //declare and grab our bottom navigation view
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+
+        //Create an event for our navigation items to tell which one is clicked and then execute some code dependant on the item
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                //We use a switch statement to check which one is clicked
+                switch (item.getItemId()){
+                    case R.id.nav_home:
+                        break;
+                    case R.id.nav_favourites:
+                        break;
+                    case R.id.nav_search:
+                        break;
+                    case R.id.nav_news:
+                        break;
+                    case R.id.nav_profile:
+                        break;
+                }
+
+                return false;
+            }
+        });
     }
+
+
+
 }
