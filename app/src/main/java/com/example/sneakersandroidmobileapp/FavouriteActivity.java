447073d7410057cf9467ead86ba7c925801f30a3
Nathanael Meshesha <nathanael.meshesha@yahoo.com>
2020-11-16 05:13:14 -0600

    Updated manifest file with out new activity layout files we created.

--- app/src/main/java/com/example/sneakersandroidmobileapp/FavouriteActivity.java
+++ app/src/main/java/com/example/sneakersandroidmobileapp/FavouriteActivity.java
@@ -1,14 +1,19 @@
 package com.example.sneakersandroidmobileapp;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class FavouriteActivity extends AppCompatActivity {
+
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favourite);
+
+        TextView title = (TextView) findViewById(R.id.favouriteActivityTitle);
     }
 }
